939ecfa56b4b32d2c1064c7ac397d4a209356007
lored <lored@DESKTOP-13RT34K.station>
2019-10-10 16:45:35 +0200

    Versione implementata senza ammettere duplicati dalla put

--- Progetto PR2/src/source/SecureFileContainer_Impl1.java
+++ Progetto PR2/src/source/SecureFileContainer_Impl1.java
@@ -6,26 +6,31 @@ import java.util.ArrayList;
 
 public class SecureFileContainer_Impl1<E> implements SecureFileContainer<E> {
 	
-	private HashMap<String, String> security;
-	private HashMap<String, ArrayList<E>> data;
+	private HashMap<String, String> security; // struttura dati per il controllo degli accessi ai dati, attraverso l'associazione utente password
+	private HashMap<String, ArrayList<File<E>>> data; // struttura dati per il salvataggio dei file relativi ad un utente
 	
 	public SecureFileContainer_Impl1() {
 		security = new HashMap<String, String>(0);
-		data = new HashMap<String, ArrayList<E>>(0);
+		data = new HashMap<String, ArrayList<File<E>>>(0);
 	}
 
 
-	public void createUser(String Id, String passw) throws NullPointerException, UserAlreadyRegisteredException, WeakPasswordException {
+	public void createUser(String Id, String passw) throws NullPointerException, UserAlreadyRegisteredException, WeakPasswordException, IllegalUsernameException {
 		
 		if (Id==null || passw==null)
 			throw new NullPointerException();
+		
 		if (security.containsKey(Id))
-			throw new UserAlreadyRegisteredException();
+			throw new UserAlreadyRegisteredException(Id + " è già registrato!");
+		
 		if (passw.length() < 5)
-			throw new WeakPasswordException();
+			throw new WeakPasswordException("La tua password è troppo corta!");
+		
+		if (Id.length() < 2)
+			throw new IllegalUsernameException("Il tuo username è troppo corto!");
 		
 	    security.put(Id, passw);
-        data.put(Id, new ArrayList<>());	
+        data.put(Id, new ArrayList<>(0));	
 	}
 
 
@@ -33,40 +38,141 @@ public class SecureFileContainer_Impl1<E> implements SecureFileContainer<E> {
 		
 		if (Owner==null || passw==null)
 			throw new NullPointerException();
+		
 		if (!security.containsKey(Owner))
-			throw new UserNotFoundException();
+			throw new UserNotFoundException("L'utente non è registrato al file storage!");
+		
 		if (!security.get(Owner).equals(passw))
-			throw new WrongPasswordException();
+			throw new WrongPasswordException("Hai inserito una password sbagliata!");
 		
 		return data.get(Owner).size();
 		
 	}
 
-	@Override
-	public boolean put(String Owner, String passw, E file)
-			throws NullPointerException, UserNotFoundException, WrongPasswordException {
-		// TODO Auto-generated method stub
-		return false;
+
+	public boolean put(String Owner, String passw, E file) throws NullPointerException, UserNotFoundException, WrongPasswordException {
+		
+		if (Owner==null || passw==null || file==null)
+			throw new NullPointerException();
+		
+		if (!security.containsKey(Owner))
+			throw new UserNotFoundException("L'utente non è registrato al file storage!");
+		
+		if (!security.get(Owner).equals(passw))
+			throw new WrongPasswordException("Hai inserito una password sbagliata!");
+		
+		if(data.get(Owner).size()==0) {
+			data.get(Owner).add(new MyFile<E>(Owner, file));
+			return true;
+		}
+		
+		E temp =  data.get(Owner).get(0).getData();
+		int i=1;
+		
+		while(!temp.equals(file) && i<data.get(Owner).size()){
+			temp = data.get(Owner).get(i).getData();
+			i++;
+		}
+		
+		if (temp!=file)
+			data.get(Owner).add(new MyFile<E>(Owner, file));
+		else
+			System.out.println("Il tuo file era già salvato nello storage, non è stata apportata alcuna modifica");
+		
+		return true;
+		
 	}
 
-	@Override
-	public E get(String Owner, String passw, E file)
-			throws NullPointerException, UserNotFoundException, WrongPasswordException, NoDataException {
-		// TODO Auto-generated method stub
-		return null;
+	public E get(String Owner, String passw, E file) throws NullPointerException, UserNotFoundException, WrongPasswordException, NoDataException {
+
+		if (Owner==null || passw==null || file==null)
+			throw new NullPointerException();
+		
+		if (!security.containsKey(Owner))
+			throw new UserNotFoundException("L'utente non è registrato al file storage!");
+		
+		if (!security.get(Owner).equals(passw))
+			throw new WrongPasswordException("Hai inserito una password sbagliata!");
+		
+		if (data.get(Owner).size()==0)
+			throw new NoDataException("Il tuo file storage è vuoto!");
+		
+		E temp =  data.get(Owner).get(0).getData();
+		int i=1;
+		
+		while(!temp.equals(file) && i<data.get(Owner).size()){
+			temp = data.get(Owner).get(i).getData();
+			i++;
+		}
+		
+		
+		if (temp!=file)
+			throw new NoDataException("Il file da te richiesto non esiste nel file storage!");
+		else
+			return temp;
+		
 	}
 
-	@Override
-	public E remove(String Owner, String passw, E file)
-			throws NullPointerException, UserNotFoundException, WrongPasswordException, NoDataException {
-		// TODO Auto-generated method stub
-		return null;
+
+	public E remove(String Owner, String passw, E file) throws NullPointerException, UserNotFoundException, WrongPasswordException, NoDataException {
+		
+		if (Owner==null || passw==null || file==null)
+			throw new NullPointerException();
+		
+		if (!security.containsKey(Owner))
+			throw new UserNotFoundException("L'utente non è registrato al file storage!");
+		
+		if (!security.get(Owner).equals(passw))
+			throw new WrongPasswordException("Hai inserito una password sbagliata!");
+		
+		if (data.get(Owner).size()==0)
+			throw new NoDataException("Il tuo file storage è vuoto!");
+		
+		E temp =  data.get(Owner).get(0).getData();
+		int i=1;
+		
+		while(!temp.equals(file) && i<data.get(Owner).size()){
+			temp = data.get(Owner).get(i).getData();
+			i++;
+		}
+		
+		
+		if (temp!=file)
+			throw new NoDataException("Il file che vuoi rimuovere non esiste nel file storage!");
+		else
+			data.get(Owner).remove(data.get(Owner).get(i-1));
+		
+		return file;
 	}
 
-	@Override
-	public void copy(String Owner, String passw, E file)
-			throws NullPointerException, UserNotFoundException, WrongPasswordException, NoDataException {
-		// TODO Auto-generated method stub
+
+	public void copy(String Owner, String passw, E file) throws NullPointerException, UserNotFoundException, WrongPasswordException, NoDataException {
+		
+		if (Owner==null || passw==null || file==null)
+			throw new NullPointerException();
+		
+		if (!security.containsKey(Owner))
+			throw new UserNotFoundException("L'utente non è registrato al file storage!");
+		
+		if (!security.get(Owner).equals(passw))
+			throw new WrongPasswordException("Hai inserito una password sbagliata!");
+		
+		if (data.get(Owner).size()==0)
+			throw new NoDataException("Il tuo file storage è vuoto!");
+		
+		E temp =  data.get(Owner).get(0).getData();
+		int i=1;
+		
+		while(!temp.equals(file) && i<data.get(Owner).size()){
+			temp = data.get(Owner).get(i).getData();
+			i++;
+		}
+		
+		
+		if (temp!=file)
+			throw new NoDataException("Il file che vuoi copiare non esiste nel file storage!");
+		else 
+			data.get(Owner).add(new MyFile<E> (Owner, file));
 		
 	}
 
