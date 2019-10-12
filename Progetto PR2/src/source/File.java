6af22a7f51479c4b64f15f1875d6c3b95554abe4
lored <lored@DESKTOP-13RT34K.station>
2019-10-12 12:12:43 +0200

    modificata intestazione specifica File<E>

--- Progetto PR2/src/source/File.java
+++ Progetto PR2/src/source/File.java
@@ -3,9 +3,13 @@ package source;
 public interface File<E> {
 	
 	/*
-	 * OVERVIEW: L'oggetto di tipo File rappresenta un tipo di dato astratto volto a contenere E e le informazioni di accesso proprie di E
+	 * OVERVIEW: L'oggetto di tipo File<E> rappresenta un tipo di dato astratto volto a contenere E e le informazioni di accesso proprie di E
 	 * 
-	 * TYPICAL ELEMENT: <E, Owner, SharedW, SharedR>
+	 * TYPICAL ELEMENT: <E, Owner, SharedW, SharedR> 
+	 * 					 E: il dato E da incapsulare
+	 * 					 Owner: il possessore del dato E
+	 * 					 SharedW: l'elenco di utenti a cui è consentito l'accesso in lettura/scrittura al dato E
+	 * 					 SharedR: l'elenco di utenti a cui è consentito l'accesso in lettura al dato E
 	 */
 	
 	public String getOwner();
